90cb93a3c59cf209198490ff0c172f4b1c8c8281
koval01 <yaroslav@koval.page>
2022-09-13 19:52:39 +0200

    Add payments method

--- modules/easydonate.py
+++ modules/easydonate.py
@@ -2,6 +2,16 @@ import os
 
 import aiohttp
 from pydantic import BaseModel
+from modules.special import StringEditor
+
+from modules.models import coupon as coupon_model
+from modules.models.coupon import ResponseItem as ResponseItemCoupon
+
+from modules.models import services as services_model
+from modules.models.services import ResponseItem as ResponseItemServices
+
+from modules.models import payment as payment_model
+from modules.models.payment import ResponseItem as ResponseItemPayment
 
 
 class EasyDonate:
@@ -18,6 +28,11 @@ class EasyDonate:
             code: str
             token: str
 
+        class Payment(BaseModel):
+            payment_id: int
+            token: str
+            tokens_send: bool
+
     @staticmethod
     async def request(headers: dict, path: str, params: dict = None) -> dict:
         async with aiohttp.ClientSession() as session:
@@ -30,40 +45,100 @@ class EasyDonate:
     class Services:
 
         @staticmethod
-        def _products(response: list) -> list:
+        def _products(response: list[ResponseItemServices]) -> list:
             return [{
-                "id": product["id"],
-                "name": product["name"],
-                "description": product["description"],
-                "image": product["image"],
-                "price": product["price"],
-                "old_price": product["old_price"],
-                "type": product["type"],
-                "number": product["number"]
-            } for product in response if not product["is_hidden"]]
+                "id": product.id,
+                "name": product.name,
+                "description": product.description,
+                "image": product.image,
+                "price": product.price,
+                "old_price": product.old_price,
+                "type": product.type,
+                "number": product.number
+            } for product in response if not product.is_hidden]
 
         async def get(self) -> list:
             response = await EasyDonate.request({
                 "Shop-Key": EasyDonate.api_key
             }, "shop/products")
-            if response["success"]:
-                return self._products(response["response"])
+            data = services_model.Model(**response)
+            if data.success:
+                return self._products(data.response)
             return []
 
-    # @staticmethod
-    # def _check_key(array: list, key: str, value: str) -> bool:
-    #     return True in [True for k in array if k[key] == value]
-    #
-    # def _coupon_response_build(self, response: list, coupon: str) -> dict:
-    #     select_coupon = self._check_key(response, "code", coupon)
-    #     if select_coupon:
-    #         pass
-    #     return {}
-    #
-    # async def coupon_check(self, coupon: str) -> dict:
-    #     response = await self._request({
-    #         "Shop-Key": self.api_key
-    #     }, "shop/coupons", params={
-    #         "where_active": "true"
-    #     })
-    #     return {}
+    class Coupon:
+
+        @staticmethod
+        def _select_coupon(data: list[ResponseItemCoupon], name: str) -> list[ResponseItemCoupon] or None:
+            result = [c for c in data if c.code == name] if len(data) else None
+            return result[0] if result else {}
+
+        @staticmethod
+        def _builder(data: ResponseItemCoupon) -> dict:
+            products = [
+                {
+                    "id": p.id, "name": p.name
+                } for p in data.products
+            ] if data else []
+
+            return {
+                "code": data.code,
+                "discount": data.sale,
+                "products": products
+            } if data else {}
+
+        async def get(self, coupon: str) -> dict:
+            response = await EasyDonate.request({
+                "Shop-Key": EasyDonate.api_key
+            }, "shop/coupons", params={
+                "where_active": "true"
+            })
+
+            data = coupon_model.Model(**response)
+            selected = self._select_coupon(data.response, coupon) \
+                if data.success and len(coupon) else {}
+
+            return self._builder(selected)
+
+    class Payment:
+
+        @staticmethod
+        def _get_tokens_paid(data: ResponseItemPayment, coins_mode: bool) -> int:
+            if coins_mode:
+                pattern = data.products[0].commands[0]
+                exc_com = data.sent_commands[0].command
+
+                ground_pattern = pattern.split("\x20")
+                ground_exc_com = exc_com.split("\x20")
+
+                for i, el in enumerate(ground_pattern):
+                    if el == "{amount}":
+                        return int(ground_exc_com[i])
+
+            return 0
+
+        @staticmethod
+        def _status(status: int) -> bool:
+            return True if status == 2 else False
+
+        def _builder(self, data: ResponseItemPayment, coins_mode: bool) -> dict:
+            return {
+                "id": data.id,
+                "customer": data.customer,
+                "email": StringEditor.censor_email(data.email),
+                "created_at": data.created_at,
+                "payment_system": data.payment_system,
+                "status": self._status(data.status),
+                "enrolled": self._get_tokens_paid(data, coins_mode)
+            }
+
+        async def get(self, bill_id: int, coins_mode: bool = True) -> dict:
+            response = await EasyDonate.request({
+                "Shop-Key": EasyDonate.api_key
+            }, f"shop/payment/{bill_id}")
+
+            data = payment_model.Model(**response)
+
+            return self._builder(data.response, coins_mode=coins_mode) \
+                if data.success else {}
+
