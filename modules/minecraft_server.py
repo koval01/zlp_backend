51cd20e7ff9aaa933b12e5dd0091f7473a7dcf79
koval01 <yaroslav@koval.page>
2022-09-13 15:06:46 +0200

    Add exception for game server status

--- modules/minecraft_server.py
+++ modules/minecraft_server.py
@@ -1,11 +1,20 @@
 from mcstatus import JavaServer
+from mcstatus.pinger import PingResponse
+from logger import log
 
 
 class StatusServer:
 
     def __init__(self, host: str = "zalupa.online:25565") -> None:
         self.server = JavaServer.lookup(host)
-        self.status = self.server.status()
+
+    def status(self) -> PingResponse or None:
+        try:
+            return self.server.status()
+        except Exception as e:
+            log.warning("Failed to get game server status. Exception: %s" % e)
 
     def online(self) -> dict:
-        return {"online": self.status.players.online}
+        status = self.status()
+        return {"online": status.players.online} \
+            if status else {}
