3b74c9313aa072118cd9ca06c76dafa5dad23545
koval01 <yaroslav@koval.page>
2022-09-14 20:07:18 +0200

    init sqlalchemy controller

---
+++ database/controller.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+Base = declarative_base()
+
+engine = create_async_engine(
+    os.getenv("DATABASE_URL")
+)
+
+async_session = AsyncSession(engine, expire_on_commit=False)
+
+async_sql_session = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
